dac4b1860dc0d81c308c5f37c84227595bf5cf0f
Juraj Martinka <jumarko@gmail.com>
2017-05-10 15:16:34 +0200

    Simplify implementation of java namespace.
    Add only three public functions: jinfo, jmethods, and jconstructors.
    Most useful one is probably jmethods, which also provideds 2-arity version
    that can be used to include deprecated methods.

--- src/clojure_repl/java.clj
+++ src/clojure_repl/java.clj
@@ -1,93 +1,52 @@
 (ns clojure-repl.java
   "Useful functions for interacting with java classes/objects.
   You can use these functions to explore public API of java classes,
-  especially `jmembers`, `jmethods`, `jconstructors`"
-  (:require [clojure.string :refer [join lower-case]]))
+  especially `jmethods` and `jinfo` can be useful."
+  (:require [clojure.string :refer [join trim]]))
 
-(defn- subs-after-last-dot [s]
-  (let [to-string (str s)]
-    (if (= -1 (.indexOf to-string "."))
-      to-string (last (re-find #".*\.([^.]+)" to-string))))) 
-(defn- member-type
-  "Returns a string description of member type - e.g. 'Field', 'Method', 'Constructor'."
-  [member]
-  (subs-after-last-dot (type member)))
+(defn- deprecated?
+  "Returns true if given method is deprecated, false otherwise.
+  Only methods marked with `java.lang.Deprecated` annotation are recognized as deprecated."
+  [method]
+  (.isAnnotationPresent method java.lang.Deprecated))
 
-(defn- normalized-name [member]
-  (subs-after-last-dot (:name member)))
+(defn- method-description
+  "Returns a text description of java.lang.reflect.Method."
+  [method]
+  (->> [(.getName method)
+        (java.util.Arrays/toString (.getParameterTypes method))
+        "->"
+        (.getReturnType method)
+        (when (deprecated? method) "(DEPRECATED)")]
+       (join " ")
+       trim))
 
-
-;;; PUBLIC STUFF
-
-(defn member-as-string
-  "Returns a string descriptions of given java object/class member.
-  For more info see `get-members`."
-  [member]
-  (join
-   " "
-   [(normalized-name member)
-    (:parameter-types member)
-    (when-let [return-type (:return-type member)]
-      (str "-> " (:return-type member)))]))
-
-(defn get-members
-  "Returns a sequence of all (by default) public members of given java object/classs.
-  The structure of items in collection is determined by what the `clojure.reflect/reflect` returns.
-  The items are sorted by name alphabetically.
-  The 1-arity variant will return all public methods.
-  The 2-arity variant will return all methods with given visibility.
-  The 3-arity variant will return all members with type tag from given set of member-types and given visibility (e.g. :public or :private).
-
-  Example: get all public constructors of java class
-    `(get-members java.util.ArrayList :public #{\"constructor\"})`"
-  ([object]
-   (get-members object :public))
-  ([object visibility]
-   (get-members object visibility #{"method"}))
-  ([object visibility member-types]
-   (->> (clojure.reflect/reflect object)
-        :members
-        (filter (fn [member] (and ((:flags member) visibility)
-                                  ((set (map lower-case member-types))
-                                   (lower-case (member-type member))))))
-        (sort-by :name))))
-
-(defn print-members
-  "Generic method for printing members of java object/class.
-  This will will just delegate to the `get-members`
-  and print each member's string represetnation on a new line."
-  [& args]
-  (let [ms (apply get-members args)
-        ms-str (map member-as-string ms)]
-    (doseq [m ms-str]
-      (println m))))
-
-
-;;; Following functions are the ones that should be used in most cases
-;;; They are simple to use and print important information.
-
-(defn jfields
-  "Prints all public fields of given java object/class."
-  [object]
-  (print-members object :public #{"field"}))
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Public API
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defn jconstructors
-  "Prints all public constructors of given java object/class."
-  [object]
-  (print-members object :public #{"constructor"}))
+  "Returns a sequence of all constructors of given java class."
+  [clazz]
+  (map str (:constructors (bean clazz))))
 
 (defn jmethods
-  "Prints all public methods of given java object/class."
-  [object]
-  (print-members object :public #{"method"}))
+  "Returns a sequence of all _public_ methods of given java class
+  whether directly declared or inherited from parent(s).
+  Deprecated methods are not included by default.
+  You can use 2-arity version to include deprecated method."
+  ([clazz]
+   (jmethods clazz false))
+  ([clazz include-deprecated?]
+   (->> (:methods (bean clazz))
+        (filter #(or
+                  include-deprecated?
+                  (not (deprecated? %))))
+        (sort-by #(.getName %))
+        (map method-description))))
 
-(defn jmembers
-  "Prints all public members of given java object/class."
-  [object]
-  (println "\nFields:")
-  (jfields object)
-  (println "\nCtors:")
-  (jconstructors object)
-  (println "\nMethods:")
-  (jmethods object))
-
+(defn jinfo
+  "Shows all public constructors and methods of given java class"
+  [clazz]
+  (concat
+   (jconstructors clazz)
+   (jmethods clazz)))
